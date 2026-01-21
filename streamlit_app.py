2c5cc5b672ae55dea299cd5f2ac15b9c600878cf
Lovely1 Solutions <aswestwood@gmail.com>
2026-01-20 22:58:30 -0800

    Update streamlit_app.py
    update a mainpage strategy

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Lovely1 Solutions")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+    "Click on the application below that you are testing."
+
+"Be very detailed in documenting errors and suggested updates. All feedback is appreciated."
 )
